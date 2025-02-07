f4149209c145ca14b1d5447441f9beb9c67385f4
origemjhanpoll <origemjhanpoll@gmail.com>
2025-02-07 15:37:08 -0300

    feat: adding new response

--- lib/app/viewmodel/motels_viewmodel.dart
+++ lib/app/viewmodel/motels_viewmodel.dart
@@ -21,7 +21,7 @@ class MotelsViewModel extends Bloc<MotelsEvent, MotelsState> {
       if (motels.isEmpty) {
         emit(MotelsEmpty());
       } else {
-        emit(MotelsLoaded([...motels, ...motels, ...motels, ...motels]));
+        emit(MotelsLoaded(motels));
       }
     } catch (e) {
       emit(MotelsError("Erro ao buscar motéis: $e"));
